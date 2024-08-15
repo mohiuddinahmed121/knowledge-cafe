8719facb4c3f47e09714f0239f5c5c6c0fd8c694
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2024-08-15 13:50:53 +0600

    js functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,28 @@ import Header from './components/Header/Header'
 
 function App() {
   const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0);
 
   const handleAddToBookmark = blog => {
     const newBookmarks = [...bookmarks, blog];
     setBookmarks(newBookmarks);
   }
+
+  const handleMarkAsRead = (id, time) => {
+    const newReadintTime = readingTime + time;
+    setReadingTime(newReadintTime);
+    // remove the read blog from book
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id != id);
+    setBookmarks(remainingBookmarks);
+  }
+
   return (
     <>
       <Header></Header>
       <div className='md:flex max-w-7xl mx-auto'>
-        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
-        <Bookmarks bookmarks={bookmarks}></Bookmarks>
+        <Blogs handleAddToBookmark={handleAddToBookmark}
+          handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
       </div>
     </>
   )
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,20 +1,24 @@
 import PropTypes from 'prop-types';
 import Bookmark from '../Bookmark/Bookmark';
 
-const Bookmarks = ({ bookmarks }) => {
+const Bookmarks = ({ bookmarks, readingTime }) => {
 
     return (
         <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4" >
+            <div className='text-blue-500 p-4 m-4 text-center bg-white border-blue-500 rounded-xl'>
+                <h3 className='text-4xl'>Reading Time: {readingTime}</h3>
+            </div>
             <h2 className="text-3xl text-center">Bookmarked Blogs: {bookmarks.length}</h2>
             {
-                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+                bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
             }
         </div>
     )
 }
 
 Bookmarks.propTypes = {
-    bookmarks: PropTypes.array
+    bookmarks: PropTypes.array,
+    readingTime: PropTypes.number
 };
 
 export default Bookmarks;
