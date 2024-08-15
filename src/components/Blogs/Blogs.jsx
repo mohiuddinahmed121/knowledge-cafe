56834a4f180a5e214f4f6952c3719f321bebb50d
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2024-08-15 10:50:30 +0600

    blog section added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookmark }) => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
@@ -11,8 +12,15 @@ const Blogs = () => {
     }, [])
 
     return (
-        <div>
-
+        <div className="md:w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                    handleAddToBookmark={handleAddToBookmark}
+                ></Blog>)
+            }
         </div>
     );
 };
